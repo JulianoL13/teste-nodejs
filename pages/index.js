51a931863e18f4042431af60e8f39b48119e51d2
JulianoL13 <125844980+JulianoL13@users.noreply.github.com>
2024-10-04 19:47:18 +0000

    alteração na frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-return <h1>Amor, só queria dizer que gosto muito de você, e dedico esse pequeno pedaço de internet para você</h1>
+return <h1>Amor, só queria dizer que gosto muito de você, e dedico esse pequeno pedaço de internet para você!</h1>
 }
 
 export default Home;
