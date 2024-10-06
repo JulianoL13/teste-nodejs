d3ffd40f5cd539765a3264062432bceba025ebd4
JulianoL13 <125844980+JulianoL13@users.noreply.github.com>
2024-10-06 18:55:50 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
-function Home(){
-return <h1>Amor, só queria dizer que gosto muito de você, e dedico esse pequeno pedaço de internet para você!</h1>
+function Home() {
+  return (
+    <h1>
+      Amor, só queria dizer que gosto muito de você, e dedico esse pequeno
+      pedaço de internet para você!
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
